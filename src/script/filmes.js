4b5586e3241bf1a76a7a63e1a698e30ce25af764
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-14 10:08:28 -0300

    avançando no consumo da api

--- src/script/filmes.js
+++ src/script/filmes.js
@@ -1,29 +1,35 @@
-var filmeEscolhido = []
-
 async function idFilme(filme) {
-    // var mensagemErro = document.getElementById("erro")
-    // mensagemErro.innerHTML = "";
+    var mensagemErro = document.getElementById("erro")
+    mensagemErro.innerHTML = "";
     try {
         var consultaId = await fetch(`https://imdb-api.com/en/API/SearchMovie/k_o545bjp7/${filme}`)
         var idEditado = await consultaId.json();
-        console.log(idEditado)
-
         var id = idEditado.results[0].id;
-        console.log(id)
+
     } catch (erro) {
         alert("filme nao encontrado")
+
     } finally {
     var consultaFilme = await fetch(`https://imdb-api.com/en/API/Wikipedia/k_o545bjp7/${id}`);
     var filmeEditado = await consultaFilme.json();
-    console.log(filmeEditado)
+
+    var consultaNota = await fetch(`https://imdb-api.com/en/API/Ratings/k_o545bjp7/${id}`)
+    var notaEditado = await consultaNota.json();
     
     var titulo = document.getElementById("titulo")
-    var ano = document.getElementById("ano")
+    var nota = document.getElementById("nota")
     var poster = document.getElementById("poster")
     var sinopse = document.getElementById("sinopse")
-    
-    titulo.innerHTML = filmeEditado.title
-    ano.innerHTML = filmeEditado.year
+
+    // var novoCartao = {
+    //     tituloFilme: filmeEditado.fullTitle,
+    //     notaFilme: "Nota IMDb: " + notaEditado.imDb,
+    //     posterFilme: idEditado.results[0].image,
+    //     sinopseFilme: filmeEditado.plotShort.html
+    // }
+
+    titulo.innerHTML = filmeEditado.fullTitle
+    nota.innerHTML = "Nota IMDb: " + notaEditado.imDb
     poster.src = idEditado.results[0].image
     sinopse.innerHTML = filmeEditado.plotShort.html
     }
@@ -32,10 +38,3 @@ async function idFilme(filme) {
 var filme = document.getElementById("titulo-filme")
 var busca = document.getElementById("btn-busca")
 busca.addEventListener("click", () => idFilme(filme.value))
-
-
-// var filme = {id: "", 
-//     titulo: "", 
-//     ano: "",    
-//     imagem: "", 
-//     sinopse: ""}
